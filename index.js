fef99c6964bcad5b6e5b2ee53e1518dfacdc44d6
Ben McClure <ben.mcclure@gmail.com>
2016-06-25 14:54:04 -0500

    First version, still untested

--- index.js
+++ index.js
@@ -2,20 +2,7 @@
  * @author bmcclure
  */
 
-var deploytool = require('deploytool');
-
 module.exports = {
-  deploy: function (environment, commit, callback) {
-    if (typeof environment === 'string') {
-      environment = deploytool.environment.load(environment);
-    }
-
-    environment.applyDefaults({
-      type: 'ftp'
-    });
-
-    var config = environment.config;
-
-    // Deploy based on diff list
-  }
+  deploy: require('./lib/deploy'),
+  ftpWrapper: require('./lib/ftp-wrapper')
 };
